750ba98a455354ebe2a90cd61b91a9ec92efece1
AJSTINGER98 <amann.jha@gmail.com>
2020-02-24 11:32:55 +0530

    supervisor table added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,55 @@
 newFunction();
 
 function newFunction() {
-    $(document).ready(function(){
+    $(document).ready(function () {
+        
+        //Search input dropdown
+        
         $('.search-bar').hide();
         $('.search-filter').click(function () {
-            $('.search-bar').toggle(700);
+            $('.search-bar').toggle(400);
         });
+
+        // Form dropdown
+        
+        $('.dropDown').hide();
+        $('.drop-down-btn').click(function () {
+            $('.dropDown').toggle(700);
+        });
+
+        //Search Filter 
+
+        $(".search-bar input").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            $(".myTable tr").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            });
+        });
+
     });
+
+    $(function()
+{
+    $(document).on('click', '.btn-add', function(e)
+    {
+            e.preventDefault();
+
+        var controlForm = $('.controls .form-group:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<i class="fas fa-minus"></i>');
+    }).on('click', '.btn-remove', function(e)
+    {
+		$(this).parents('.entry:first').remove();
+
+		e.preventDefault();
+		return false;
+	});
+});
+
 }
