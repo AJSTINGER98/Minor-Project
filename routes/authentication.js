9e9da4070b2b8e98356251812ec6cbce0326804d
NoobMaster98 <abhishek.bhushan10@gmail.com>
2020-05-23 21:48:34 +0530

    update authentication.js

--- routes/authentication.js
+++ routes/authentication.js
@@ -52,7 +52,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req,res) => {
     req.logout();
-    res.redirect('/login');
+    res.redirect('/');
 });
 
 module.exports = router;
